4b936f10ff3d1803864320deca6a9428c137b458
n273b <nishthabarnwal273@gmail.com>
2024-03-02 19:49:26 +0530

    Installed all dependencies for Backend

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+// import express from 'express';
+
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const ShortUrl = require('./models/shortUrl')
+
+const path = require('path')
+
+// Serve static files from the "src" directory
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Route for handling requests to the .jsx file
+app.get('/', (req, res) => {
+    res.sendFile('./url-shortener/vite-project/src/main.jsx', { root: __dirname })
+})
+
+app.listen(process.env.PORT || 5000)
